265833774973f99f9e13c24d4fcb22976dfe42a4
Reich Mate <reichmate21@gmail.com>
2026-01-22 19:54:50 +0200

    Game file variables addition

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Game {
+
+    private final Board board;
+    private final Player human;
+    private final Player computer;
+    private Player currentPlayer;
+
+}
